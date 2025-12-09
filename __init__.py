66855cbeb04f30a1039c219e94f6a30ee79157e0
Vasiliy Sibirenkov <sbrdigital@yandex.ru>
2025-12-09 03:02:27 +0300

    Добавлен импорт

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+""" Содержит асинхронный клиент для работы с HTTP запросами. """
 
+from .aiohttp_client import AiohttpClient
+
+
+# Экспортируемый интерфейс модуля
+__all__ = [
+    "AiohttpClient",
+]
